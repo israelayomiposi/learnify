f311ceb0ff7fd5817b88f5b9b81729619d1fdc93
israelayomiposi <oladipupoisrael181@gmail.com>
2025-11-25 00:53:10 +0100

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,25 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-<<<<<<< HEAD
-  baseURL: "https://learnify-8.onrender.com/api",
-});
-
-// Attach JWT token to all requests
-=======
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
-// Attach JWT token
->>>>>>> 3abb815 (updated)
+// Attach JWT token to all requests
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
-<<<<<<< HEAD
-  
-=======
->>>>>>> 3abb815 (updated)
 });
 
 export default API;
